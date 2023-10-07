e63976108c23dc320a4fce644fb44d85d510ef42
IreFri <irenefrizza@gmail.com>
2023-10-08 08:38:54 +0900

    Fix

--- src/states/SoftFootState.cpp
+++ src/states/SoftFootState.cpp
@@ -1624,10 +1624,10 @@ void SoftFootState::updateFootSwingPose(mc_control::fsm::Controller & ctl, const
   {
     desired_offset_position_x = 0.;
   }
-  double desired_offset_position_z = foot_data_[current_moving_foot].position_offset_z;
+  double desired_offset_position_z = foot_data_[current_moving_foot].position_offset_z * 1.15;
   if(desired_angle > 0.05)
   {
-    desired_offset_position_z *= 1.5 + 0.015;
+    desired_offset_position_z = desired_offset_position_z * 1.30 + 0.015;
   }
   if(!with_foot_adjustment_ && !with_ankle_rotation_)
   {
